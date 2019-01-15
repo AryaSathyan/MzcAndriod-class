3f46320c64719d1908d6a09e88ad57698654f304
AryaSathyan <aryasathyan667@gmail.com>
2019-01-15 11:45:55 +0530

    button click completed

--- app/src/main/java/com/example/administrator/mzcapplication/MainActivity.java
+++ app/src/main/java/com/example/administrator/mzcapplication/MainActivity.java
@@ -2,12 +2,34 @@ package com.example.administrator.mzcapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
+EditText ed1,ed2;
+    Button bu;
+    String getuname,getpassword;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ed1=(EditText)findViewById(R.id.user);
+        ed2=(EditText)findViewById(R.id.pass);
+        bu=(Button)findViewById(R.id.login);
+
+        bu.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                getuname=ed1.getText().toString();
+                getpassword=ed2.getText().toString();
+                Toast.makeText(getApplicationContext(),getuname,Toast.LENGTH_SHORT).show();
+                Toast.makeText(getApplicationContext(),getpassword,Toast.LENGTH_SHORT).show();
+
+                //Toast.makeText(getApplicationContext(),"HELLO",Toast.LENGTH_LONG).show();
+            }
+        });
+
     }
 }
